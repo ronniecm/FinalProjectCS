0763109d168cd4ea4e21c712a95dd56214246ce8
zwols <zwols@LAPTOP-G9RKIUJ9.fios-router.home>
2019-03-24 19:49:32 -0400

    Added Text Box and EventListeners to remove method although an error prevents the code from running.

--- src/HomeView.java
+++ src/HomeView.java
@@ -1,3 +1,7 @@
+
+// Credits: https://docs.oracle.com/javase/tutorial/uiswing/components/list.html#mutable
+//			https://www.dreamincode.net/forums/topic/339909-text-field-text-overlay/
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
@@ -13,11 +17,17 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.JTextField;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class HomeView extends JFrame
 {
 	private DefaultListModel listModel = new DefaultListModel();
 	private JPanel contentPane;
+	private JTextField userText;
 
 	/**
 	 * Launch the application.
@@ -53,26 +63,57 @@ public class HomeView extends JFrame
 		setContentPane(contentPane);
 
 		JList playlistList = new JList(listModel);
-		
+
 		contentPane.add(playlistList, BorderLayout.CENTER);
 
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.SOUTH);
-		panel.setLayout(new GridLayout(1, 0, 0, 0));
+		panel.setLayout(new GridLayout(0, 1, 0, 0));
+
+		userText = new JTextField();
+		userText.setText("Enter PlayList Name Here");
+		userText.addFocusListener(new FocusAdapter()
+		{
+			public void focusGained(FocusEvent arg0)
+			{
+				if (userText.getText().equals("Enter Playlist Name Here"))
+					userText.setText("");
+			}
+
+			public void focusLost(FocusEvent e)
+			{
+				if (userText.getText().equals(""))
+					userText.setText("Enter Playlist Name Here");
+			}
+		});
+
+		panel.add(userText);
+		userText.setColumns(10);
+
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1);
+		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
 
 		JButton addPlaylistBtn = new JButton("Add Playlist");
+		panel_1.add(addPlaylistBtn);
+
+		JButton removePlaylistBtn = new JButton("Remove Playlist");
+		panel_1.add(removePlaylistBtn);
+		removePlaylistBtn.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent arg0)
+			{
+				listModel.remove(playlistList.getSelectedIndex());
+			}
+		});
 		addPlaylistBtn.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent arg0)
 			{
-				Playlist p = new Playlist("placeholder");//take string from another frame as the playlist name
+				Playlist p = new Playlist("placeholder");// take string from another frame as the playlist name
 				listModel.insertElementAt(p.getName(), 0);
 			}
 		});
-		panel.add(addPlaylistBtn);
-
-		JButton removePlaylistBtn = new JButton("Remove Playlist");
-		panel.add(removePlaylistBtn);
 
 		JLabel lblNewLabel = new JLabel("Playlists");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
